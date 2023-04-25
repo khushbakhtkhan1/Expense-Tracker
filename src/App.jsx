974920c791a99ffa82ce1c6c0dc8286a2c9d202f
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2023-04-25 12:16:47 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budgets from './components/Budgets';
 import Remaining from "./components/Remaining";
 import Expense from "./components/Expense";
-
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import { AppProvider } from "./context/AppContext";
 
 const App=()=>{
     return(
+        <AppProvider>
         <div className="container">
             <h1 className="mt-3">My Budget Planner</h1>
             <div className="row mt-3">
@@ -20,7 +23,20 @@ const App=()=>{
                     <Expense />
                 </div>
             </div>
+            <h3 className="mt-3">Expenses</h3>
+            <div className="row mt-3">
+                <div className="col-sm">
+                    <ExpenseList />
+                </div>
+            </div>
+            <h3 className="mt-3">Add Expense</h3>
+            <div className="row mt-3">
+                <div className="col-sm">
+                    <AddExpenseForm />
+                </div>
+            </div>
         </div>
+       </AppProvider> 
     );
 };
 
--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Expense=()=>{
+    const{expenses}=useContext(AppContext);
+    const totalExpenses=expenses.reduce((total, item)=>{
+        return(total+=item.cost);
+    },0);
     return(
         <div className='alert alert-primary'>
-            <span>Spent So Far: $1000</span>
+            <span>Spent So Far: ${totalExpenses}</span>
         </div>
     );
 };
